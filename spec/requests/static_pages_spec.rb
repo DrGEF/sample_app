30c8431a2f25d28df612a2e00fafc9a1a27d56b0
Nik <thedrgef@gmail.com>
2015-07-04 01:03:53 +0300

    finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it 'should have the right title' do
+  		visit '/static_pages/home'
+  		expect(page).to have_title('Home')
+  	end
+  end
+
+  describe "help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+    it 'should have the right title' do
+  		visit '/static_pages/help'
+  		expect(page).to have_title('Help')
+  	end
+  end
+
+  describe 'about page' do
+  	it 'should have the content "about"' do
+  		visit '/static_pages/about'
+  		expect(page).to have_content('about')
+  	end
+  	it 'should have the right title' do
+  		visit '/static_pages/about'
+  		expect(page).to have_title('About')
+  	end
+  end
+end
