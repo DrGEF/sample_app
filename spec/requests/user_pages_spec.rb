6bd09735294fd5208959590bf2c491f851c788d6
Nik <thedrgef@gmail.com>
2015-07-13 03:24:13 +0300

    add tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -35,12 +35,34 @@ describe "User pages" do
 	      end
 	    end
 
+	    describe "with invalid information" do
+	      
+	      before { visit signup_path }
+	    	
+	      describe "after submission" do
+
+	        before { click_button submit }
+
+	        it { should have_title('Sign up') }
+	        it { should have_content('error') }
+	      end
+
+	    end
+
 	    describe "with valid information" do
 	      before do
 	        fill_in "Name",         with: "Example User"
 	        fill_in "Email",        with: "user@example.com"
 	        fill_in "Password",     with: "foobar"
 	        fill_in "Confirmation", with: "foobar"
+	      end
+
+	      describe "after saving the user" do
+	        before { click_button submit }
+	        let(:user) { User.find_by(email: 'user@example.com') }
+
+	        it { should have_title(user.name) }
+	        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 	      end
 
 	      it "should create a user" do
